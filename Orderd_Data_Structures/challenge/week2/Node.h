a396160e56c5250c4e5298a3d8eeb5463d21ed06
chloe-codes1 <juhyun.kim@lindsey.edu>
2024-03-03 17:50:16 +0900

    feat: Add Ordered Data Structure week 2 challenge

---
+++ Orderd_Data_Structures/challenge/week2/Node.h
@@ -0,0 +1,11 @@
+class Node {
+ public:
+  Node *left, *right;
+  Node() { left = right = nullptr; }
+  ~Node() {
+    delete left;
+    left = nullptr;
+    delete right;
+    right = nullptr;
+  }
+};
